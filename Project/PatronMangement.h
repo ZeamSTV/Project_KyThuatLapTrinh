bfa16c17dc5ff05dd9481efebabcd0023f8ffacc
Truk <hanqhoa01@gmail.com>
2023-11-27 22:45:59 +0700

    Hoan thien Patron + BorrowingRecord

--- Project/PatronMangement.h
+++ Project/PatronMangement.h
@@ -5,7 +5,10 @@ using namespace std;
 class PatronMangement
 {
 public:
-	vector<Patron> patronList;
+	vector<Patron> patronList = { Patron(1, "truk"),
+	Patron(2, "Zink"),
+	Patron(3, "Dux"),
+	Patron(4, "Thox"), };
 public:
 	void addPatron();
 	void deletePatron();
